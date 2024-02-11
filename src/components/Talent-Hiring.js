d0e2c1d0f3c5d7d15a8f3f63fdbf22e24e780c65
Viveksingh99 <vivek22jan99@gmail.com>
2024-02-11 23:51:09 +0530

    change style

--- src/components/Talent-Hiring.js
+++ src/components/Talent-Hiring.js
@@ -3,6 +3,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Image from "next/image";
+import { Outfit } from "next/font/google";
 
 
 const Hiring = () => {
@@ -41,15 +42,15 @@ const Hiring = () => {
   return (
     <>
       <div className="text-center my-10">
-        <h3 className="text-[#E68F50]">
+        <Typography color="#E68F50" variant="h5" fontSize="40px" marginBottom="10px" fontWeight={600} fontFamily="outfit">
           Stackkaroo’s Talent Hiring & Job Placement Services
-        </h3>
-        <p>
+        </Typography>
+        <Typography variant="subtitle2" fontFamily="outfit" fontWeight={500} fontSize="22px" color="#36454F">
           We are a talent searching platform that redesigns how companies hire
           talents. It is a cost-effective solution for industries. <br /> We
           also provide global job opportunities with good salaries, support, and
           professional development.
-        </p>
+        </Typography>
       </div>
       <div className="flex flex-wrap justify-center px-5">
       {Data.map((item, index)=>(
@@ -62,10 +63,10 @@ const Hiring = () => {
             alt="Picture of the author"
             style={{ display: 'block', margin: 'auto' }}
           />
-            <Typography variant="h6" component="div" sx={{ margin: 'auto', color:'#0A56F1', marginY:'20px' }}>
+            <Typography variant="h6" color='#0A56F1' fontWeight={600} fontSize="22px" fontFamily="outfit" component="div" sx={{ margin: 'auto', marginY:'20px' }}>
              {item.title}
             </Typography>
-            <Typography variant="body2" sx={{ marginBottom:'20px'}}>
+            <Typography variant="body2" color="#36454F" fontSize="20px" fontFamily="Outfit" fontWeight={600} sx={{ marginBottom:'20px'}}>
               {item.discription}
             </Typography>
           </CardContent>
